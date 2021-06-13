0ce89359f4d76f077d5205e22c539eaebb523746
Khaly17 <diengkhaly17@gmail.com>
2021-06-13 12:11:52 +0000

    Les React Hook

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,86 +1,74 @@
 //Librairies
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 //Composants
 import Eleve from '../../components/Eleve/Eleve';
 
-class App extends Component {
+function App() {
 
-    constructor(props) {
-        super(props);
-        console.log('[App.js] Constructor');
+    // state = {
+    //     eleves: [
+    //         { nom: 'Jouahibou DIAME', moyenne: 19 },
+    //         { nom: 'Afiss DIA', moyenne: 19 },
+    //         { nom: 'ahmed Diop', moyenne: 25 }
+    //     ]
+    // }
+    const [eleves, setEleve] = useState([
+
+        { nom: 'Jouahibou DIAME', moyenne: 19 },
+        { nom: 'Afiss DIA', moyenne: 19 },
+        { nom: 'ahmed Diop', moyenne: 25 }
+    ]
+    );
+    const [maintenance, setMaintenance] = useState(false);
+
+    const buttonClickedHandler = nouveauNom => {
+        // const newStat = [...this.state.eleves];
+        // newStat[0].nom = nouveauNom
+
+        // //creation du state a partir de zero
+        // this.setState({
+        //     ...this.state,
+        //     eleves: newStat
+        // });
+        const nouveauxEleves = [...eleves];
+        nouveauxEleves[0].nom = nouveauNom;
+        setEleve(nouveauxEleves)
     }
 
-    //Monte
-    componentDidMount() {
-        console.log('[App.js] componentDidMount');
-    }
+    return (
+        <div className="App">
 
-    //Modification
-    componentDidUpdate() {
-        console.log('[App.js] componentDidUpdate')
-    }
+            <h1>Bienvenue dans la classe Terre !!</h1>
 
-    //Demonte
-    componentWillUnmount() {
-        console.log('[App.js] componentWillUnmount');
-    }
+            <button onClick={buttonClickedHandler.bind(this, "Moi")}>Transformer le premier eleve</button>
 
+            <Eleve
+                nom={eleves[0].nom}
+                moyenne={eleves[0].moyenne}
+                clic={() => buttonClickedHandler("Khaly")}
+            >
+                "Allez plus loin"
+            </Eleve>
 
-    state = {
-        eleves: [
-            { nom: 'Jouahibou DIAME', moyenne: 19 },
-            { nom: 'Afiss DIA', moyenne: 19 },
-            { nom: 'ahmed Diop', moyenne: 25 }
-        ]
-    }
+            <Eleve
+                nom={eleves[1].nom}
+                moyenne={eleves[1].moyenne}
+                clic={() => buttonClickedHandler("Omar")}
+            >
+                "La connaissance partagee "
+            </Eleve>
 
-    buttonClickedHandler = nouveauNom => {
-        const newStat = [...this.state.eleves];
-        newStat[0].nom = nouveauNom
+            <Eleve
+                nom={eleves[2].nom}
+                moyenne={eleves[2].moyenne}
+                clic={() => buttonClickedHandler("Amar")}
+            >
+                "Connais toi meme !"
+            </Eleve>
 
-        //creation du state a partir de zero
-        this.setState({
-            ...this.state,
-            eleves: newStat
-        });
-    }
-
-    render() {
-        return (
-            <div className="App">
-
-                <h1>Bienvenue dans la classe Terre !!</h1>
-
-                <button onClick={this.buttonClickedHandler.bind(this, "Moi")}>Transformer le premier eleve</button>
-
-                <Eleve
-                    nom={this.state.eleves[0].nom}
-                    moyenne={this.state.eleves[0].moyenne}
-                    clic={() => this.buttonClickedHandler("Khaly")}
-                >
-                    "Allez plus loin"
-                </Eleve>
-
-                <Eleve
-                    nom={this.state.eleves[1].nom}
-                    moyenne={this.state.eleves[1].moyenne}
-                    clic={() => this.buttonClickedHandler("Omar")}
-                >
-                    "La connaissance partagee "
-                </Eleve>
-
-                <Eleve
-                    nom={this.state.eleves[2].nom}
-                    moyenne={this.state.eleves[2].moyenne}
-                    clic={() => this.buttonClickedHandler("Amar")}
-                >
-                    "Connais toi meme !"
-                </Eleve>
-
-            </div>
-        );
-    }
+        </div>
+    );
 }
 export default App;
